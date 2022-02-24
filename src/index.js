47a2d032f2c0478bc47dabc6c71edcb557abdafa
xposed01 <43813249+xposed01@users.noreply.github.com>
2022-02-25 03:09:28 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+	const objectBrackets = Object.fromEntries(bracketsConfig);
+  
+	for (let i = 0; i < str.length; i++) {
+		let closedBracket = objectBrackets[str[i]];
+		// проверка на закрытую скобку в пустом стеке
+		if (stack.length == 0 && closedBracket == undefined) return false;
+		
+		if (stack.length > 0 || str[i] !== closedBracket) {
+			
+			if (str[i] !== closedBracket && closedBracket !== undefined) {
+				stack.push(str[i]);
+			}
+			if (closedBracket == undefined && stack.length > 0) {
+				// если закрываюзий аналог ласт скобки стака равен
+				if (objectBrackets[stack[stack.length-1]] == str[i]) {
+					stack.pop();
+				}
+			}	
+		} 
+	}
+	return stack.length == 0;
 }
